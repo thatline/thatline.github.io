4036aab8207f5a7b6bc9a778997603fceecae6d7
Shakeel Mohamed <contact@shakeel.xyz>
2021-12-05 15:55:56 -0800

    add MVP version

--- docs/scripts/lines.js
+++ docs/scripts/lines.js
@@ -1,8 +1,8 @@
-[
+var lines = [
     {
         "artist": "Estelle",
         "song": "American Boy ft. Kanye West",
-        "line": "Look at this pea coat, tell me he's broke",
+        "line": "Look at this pea coat // tell me he’s broke",
         "genius_link": "https://genius.com/91238"
     },
     {
@@ -14,7 +14,7 @@
     {
         "artist": "Kanye West",
         "song": "Junya",
-        "line": "I won with the bucks, boy // Let me Giannis // I won with the bucks, boy // The spirit is on us",
+        "line": "I won with the bucks, boy // Let me Giannis",
         "genius_link": "https://genius.com/23571738"
     },
     {
@@ -26,25 +26,25 @@
     {
         "artist": "The Weeknd",
         "song": "Tell Your Friends",
-        "line": "Cruise through the west–end in my new Benz // I'm just tryna live life through a new lens",
+        "line": "Cruise through the west–end in my new Benz // I’m just tryna live life through a new lens",
         "genius_link": "https://genius.com/7659455"
     },
     {
         "artist": "Lupe Fiasco",
         "song": "The Show Goes On",
-        "line": "Yeah, yeah, the world is yours, I was once that little boy // Terrified of the world, now I'm on a world tour",
+        "line": "I was once that little boy // Terrified of the world // now I’m on a world tour",
         "genius_link": "https://genius.com/15947465/Lupe-fiasco-the-show-goes-on/Yeah-yeah-the-world-is-yours-i-was-once-that-little-boy-terrified-of-the-world-now-im-on-a-world-tour"
     },
     {
         "artist": "Consequence",
         "song": "Grammy Family ft. Kanye West, DJ Khaled, John Legend",
-        "line": "Hi haters I'm back off hiatus // I feel just like you, I mean even I hate us",
+        "line": "Hi haters I’m back off hiatus // I feel just like you, I mean even I hate us",
         "genius_link": "https://genius.com/Consequence-grammy-family-lyrics"
     },
     {
         "artist": "Jay Z",
-        "song": "U Don't Know",
-        "line": "Put me anywhere on God's green earth, I'll triple my worth",
+        "song": "U Don’t Know",
+        "line": "Put me anywhere on God’s green earth //  I’ll triple my worth",
         "genius_link": "https://genius.com/381141"
     }
-]
+];
