19c5c885de98a5293423d6f4c4a291618717e759
Shakeel Mohamed <contact@shakeel.xyz>
2021-12-06 13:30:48 -0800

    switch to pulling in a pure JSON data file

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,5 @@ module.exports = function(eleventyConfig) {
    
     eleventyConfig.addPassthroughCopy("src/scripts");
     eleventyConfig.addPassthroughCopy("img");
+    eleventyConfig.addPassthroughCopy("src/data/lines.json");
 };
