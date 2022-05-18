898682f58ed39c2aa75f702b7c12c93c3289274f
Shakeel Mohamed <contact@shakeel.xyz>
2022-05-17 22:31:38 -0700

    add lines + a test

--- test/test_validate.js
+++ test/test_validate.js
@@ -4,15 +4,25 @@ const path = require("path");
 const Validator = require("jsonschema").Validator;
 const v = new Validator();
 
+const dataFile = JSON.parse(fs.readFileSync(path.normalize("docs/data/lines.json"), "utf8"));
+
 describe("JSON validation", function() {
-    it("lines.json", function() {
+    it("lines.json matches schema", function() {
         const linesSchema = JSON.parse(fs.readFileSync(path.normalize("test/lines.schema.json"), "utf8"));
-        const input = JSON.parse(fs.readFileSync(path.normalize("docs/data/lines.json"), "utf8"));
         
-        const res = v.validate(input, linesSchema);
+        const res = v.validate(dataFile, linesSchema);
         for (let error of res.errors) {
             console.log(error);
         }
         assert.ok(res.valid);
+    });
+    it("lines.json each entry has a unique URL", function() {
+        let urls = new Set();
+        for (let line of dataFile) {
+            // Ensure there is a url. If not, dump the entry
+            assert.ok(line.hasOwnProperty("genius_link"), JSON.stringify(line));
+            urls.add(line.genius_link);
+        }
+        assert.equal(dataFile.length, urls.size);
     });
 });
